1aa975183745b321a62391c5c386c33b99a497a4
Willian Maruno <jackmaruno@gmail.com>
2019-08-01 11:20:07 -0300

    inicial

--- protected/js/home.js
+++ protected/js/home.js
@@ -65,4 +65,134 @@ function homeCtrl($scope, $location, $http) {
 	
 	$scope.initHomeCtrl();
  
+		
+	function Service(http_, servico_){
+		var http = http_;
+		var servico = servico_;
+
+		function getUrl(path) {     
+			if(path != null && path.length > 0){
+				return servico.path + path;
+			}
+			return servico.path;
+		}  
+		
+		function pesquisar(path, listener, config) {  
+			http.get(getUrl(path), config)
+				.success(function(data, status, headers, config) { 
+					if(data.sucesso == null){ 
+						sessionExpired();
+					}else{
+						if(!data.sucesso){ 
+							showMessage(data.sucesso, data.msg);  
+						} 
+						if(listener){
+							listener(data.dados.RESPOSTA);
+						}
+					} 
+				}); 
+		} 
+
+		function salvar(path, obj, config, idModal, listener) {  
+			mpLoading.show();
+			http.post(getUrl(path), obj, config)
+				.success(function (data, status, headers, config) {
+					if(data.sucesso == null){ 
+						sessionExpired();
+						mpLoading.hide();
+					}else{
+						showMessage(data.sucesso, data.msg);  
+						mpLoading.hide();
+						if(listener){
+							listener(data.dados.RESPOSTA);
+						}
+					}
+					$('#'+idModal).modal('hide');  
+				})
+				.error(function(data, status, headers, config) { 
+					mpLoading.hide();
+					showMessage(false, data); 
+					$('#'+idModal).modal('show'); 
+				});
+		}
+		
+		function alterar(path, obj, config, idModal, listener) {  
+			mpLoading.show();
+			http.put(getUrl(path), obj, config)
+			.success(function (data, status, headers, config) {
+				if(data.sucesso == null){ 
+					sessionExpired();
+					mpLoading.hide();
+				}else{
+					showMessage(data.sucesso, data.msg);  
+					mpLoading.hide();
+					if(listener){
+						listener(data.dados.RESPOSTA);
+					}
+				}
+				$('#'+idModal).modal('hide');  
+			})
+			.error(function(data, status, headers, config) { 
+				mpLoading.hide();
+				showMessage(false, data); 
+				$('#'+idModal).modal('show'); 
+			});
+		}
+		
+		function excluir(path, config, idModal, listener) {  
+			mpLoading.show();
+			http({method: 'DELETE',url: getUrl(path), config:config})
+			.success(function (data, status, headers, config) {
+				if(data.sucesso == null){ 
+					sessionExpired();
+					mpLoading.hide();
+				}else{
+					showMessage(data.sucesso, data.msg);  
+					mpLoading.hide();
+					if(listener){
+						listener();
+					}
+				}
+				$('#'+idModal).modal('hide');  
+			})
+			.error(function(data, status, headers, config) { 
+				mpLoading.hide();
+				showMessage(false, data); 
+				$('#'+idModal).modal('show'); 
+			});
+		} 
+		
+		return {
+			'pesquisar': pesquisar
+			, 'salvar': salvar 
+			, 'alterar': alterar 
+			, 'excluir': excluir 
+		}
+	};
+	
+
+	function Parametros(http_){
+		var http = http_;  
+		
+		function pesquisar(path, listener, config) {  
+			http.get('/rest/parametros'+path, config)
+				.success(function(data, status, headers, config) {
+					if(data.sucesso == null){ 
+						sessionExpired();
+					}else{
+						if(!data.sucesso){ 
+							showMessage(data.sucesso, data.msg);  
+						} 
+						if(listener){
+							listener(data.dados.RESPOSTA);
+						}
+					}  
+				}); 
+		}   
+		
+		return {
+			'pesquisar': pesquisar 
+		,'find':find
+		}
+	};
 };
